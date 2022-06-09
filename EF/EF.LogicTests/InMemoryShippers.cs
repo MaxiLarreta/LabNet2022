acad01b3a84dc1e8de3ed85c9018fce307e9e698
MaxiLarreta <81243887+MaxiLarreta@users.noreply.github.com>
2022-06-08 23:05:35 -0300

    Agrego Unit Tests

---
+++ EF/EF.LogicTests/InMemoryShippers.cs
@@ -0,0 +1,33 @@
+﻿using EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.LogicTests
+{
+    public class InMemoryShippers
+    {
+        private List<Shippers> _db = new List<Shippers>();
+        public Exception ExceptionToThrow { get; set; }
+        public IEnumerable<Shippers> GetAllShippers()
+        {
+            return _db.ToList();
+        }
+        public Shippers GetShipperByID(int id)
+        {
+            return _db.FirstOrDefault(d => d.ShipperID == id);
+        }
+        public void CreateNewShipper(Shippers shipperToCreate)
+        {
+            if (ExceptionToThrow != null)
+                throw ExceptionToThrow;
+            _db.Add(shipperToCreate);
+        }
+        public void DeleteShipper(int id)
+        {
+            _db.Remove(GetShipperByID(id));
+        }
+    }
+}
