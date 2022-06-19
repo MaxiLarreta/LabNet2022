5437ffcb74a2c59284b34865e49cc0f7cff6eda2
MaxiLarreta <81243887+MaxiLarreta@users.noreply.github.com>
2022-06-18 23:46:52 -0300

    avance Mvc

---
+++ MVC/MVC.MVC/Controllers/ShippersController.cs
@@ -0,0 +1,30 @@
+﻿using MVC.Entities;
+using MVC.Logic;
+using MVC.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC.MVC.Controllers
+{
+    public class ShippersController : Controller
+    {
+        // GET: Shippers
+        public ActionResult Index()
+        {
+            var logic = new ShippersLogic();
+            List<Shippers> shippers = logic.GetAll();
+
+            List<ShippersView> shippersView = shippers.Select(s => new ShippersView
+            {
+                CompanyName = s.CompanyName,
+                Phone = s.Phone
+            }).ToList();
+
+            return View(shippersView);
+        }
+    }
+}
