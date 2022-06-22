6535ecb8b985c75baaba85eb7d2b7bc30138fbd5
MaxiLarreta <81243887+MaxiLarreta@users.noreply.github.com>
2022-06-22 15:18:07 -0300

    Final version

---
+++ MVC/WebAppi.Service/ShippersService.cs
@@ -0,0 +1,45 @@
+﻿using MVC.Entities;
+using MVC.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAppi.Service.Interface;
+
+namespace WebAppi.Service
+{
+    public class ShippersService : IShippersService
+    {
+        private readonly ShippersLogic _repository;
+        public ShippersService()
+        {
+            _repository = new ShippersLogic();
+        }
+
+        public void Add(Shippers shipper)
+        {
+            _repository.Add(shipper);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<Shippers> GetAll()
+        {
+           return _repository.GetAll();
+        }
+
+        public Shippers GetById(int id)
+        {
+            return _repository.FindOne(id);
+        }
+
+        public void Update(Shippers shipper)
+        {
+            _repository.Update(shipper);
+        }
+    }
+}
